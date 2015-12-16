d618d7246649f0745b2e5694cfad4bc4fcfc5c00
Yogesh Naik <yogeshrnaik@gmail.com>
2015-12-16 20:54:49 +0530

    chapter 5 - exercises

--- fp-scala-book/src/fp/chapter5/stream/Stream.scala
+++ fp-scala-book/src/fp/chapter5/stream/Stream.scala
@@ -69,6 +69,68 @@ sealed trait Stream[+A] {
     case Cons(h, t) => t().exists(p)
     case _ => false
   }
+
+
+  /** The arrow => in front of the argument type B means that
+    * the function f takes its second argument by name and may choose not to evaluate it. */
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    this match {
+      case Cons(h, t) => f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
+
+  def existsWithFoldRight(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) => p(a) || b)
+
+  /**
+   * EXERCISE 5.4
+   * Implement forAll, which checks that all elements in the Stream match a given predicate.
+   * Your implementation should terminate the traversal as soon as it encounters a nonmatching value.
+   */
+  @tailrec
+  final def forAll(p: A => Boolean): Boolean = {
+    this match {
+      case Cons(h, t) if p(h()) => t().forAll(p)
+      case Cons(h, t) => false
+      case _ => true
+    }
+  }
+
+  /**
+   * EXERCISE 5.5
+   * Use foldRight to implement takeWhile.
+   */
+  def takeWhileWithFoldRight(predicate: A => Boolean): Stream[A] = {
+    foldRight(empty: Stream[A])((h, t) => if (predicate(h)) cons(h, t) else empty)
+  }
+
+  /**
+   * EXERCISE 5.6
+   * Hard: Implement headOption using foldRight.
+   */
+  def headOptionWithFoldRight: Option[A] =
+    foldRight(None: Option[A])((h, _) => Some(h))
+
+  /**
+   * EXERCISE 5.7
+   * Implement map, filter, append, and flatMap using foldRight. The append method should be non-strict in its argument.
+   */
+  def map[B](f: A => B): Stream[B] = {
+    foldRight(empty[B])((h, t) => cons(f(h), t))
+  }
+
+  def filter(f: A => Boolean): Stream[A] = {
+    foldRight(empty[A])((h, t) => if (f(h)) cons(h, t) else t)
+  }
+
+  def append[B >: A](s: => Stream[B]): Stream[B] =
+    foldRight(s)((h, t) => cons(h, t))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] =
+    foldRight(empty[B])((h, t) => f(h) append t)
+
+  def find(p: A => Boolean): Option[A] =
+    filter(p).headOption
 }
 
 case object Empty extends Stream[Nothing]
@@ -89,6 +151,62 @@ object Stream {
 
   /** A smart constructor for creating an empty stream of a particular type. */
   def empty[A]: Stream[A] = Empty
+
+
+  /**
+   * EXERCISE 5.8
+   * Generalize ones slightly to the function constant, which returns an infinite Stream of a given value.
+   */
+  def constant[A](a: A): Stream[A] = Stream.cons(a, constant(a))
+
+  /**
+   * EXERCISE 5.9
+   * Write a function that generates an infinite stream of integers, starting from n, then n + 1, n + 2, and so on.7
+   */
+  def from(n: Int): Stream[Int] = Stream.cons(n, from(n + 1))
+
+  /**
+   * EXERCISE 5.10
+   * Write a function fibs that generates the infinite stream of Fibonacci numbers: 0, 1, 1, 2, 3, 5, 8, and so on.
+   */
+  def fibs(): Stream[Int] = {
+    def innerFibs(n1: Int, n2: Int): Stream[Int] = {
+      Stream.cons(n1, innerFibs(n2, n1 + n2))
+    }
+
+    innerFibs(0, 1)
+  }
+
+  /**
+   * EXERCISE 5.11
+   * Write a more general stream-building function called unfold. It takes an initial state,
+   * and a function for producing both the next state and the next value in the generated stream.
+   */
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = {
+    f(z) match {
+      case Some((a, s)) => cons(a, unfold(s)(f)) // f produces Option[(A, S)] i.e. a pair containing A and S
+      case None => empty
+    }
+  }
+
+  /**
+   * EXERCISE 5.12
+   * Write fibs, from, constant, and ones in terms of unfold.
+   */
+  def fibsWithUnfold(): Stream[Int] = {
+    unfold((0, 1))((pair) => pair match {
+      case (f1, f2) => Some(f1, (f2, (f1 + f2)))
+    })
+  }
+
+  def fromWithUnfold(n: Int) = {
+    unfold(n)(i => Some(i, i + 1))
+  }
+
+  def constantWithUnfold[A](a: A): Stream[A] = unfold(a)(i => Some(i, i))
+
+  def onesWithUnfold: Stream[Int] = unfold(1)(i => Some(i, i))
+
 
   /** A convenient variable-argument method for constructing a Stream from multiple elements. */
   def apply[A](as: A*): Stream[A] =
